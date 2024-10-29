48dfe55371d76ea2bf6041ed35de466108036fad
just biar <160464890+justbiar@users.noreply.github.com>
2024-10-29 22:54:11 +0300

    React kuruldu

---
+++ nodeapp/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import arfLogo from './images/icon48.png'
+import './App.css'
+
+function App() {
+    // return içine html dışına javascript yazılır.
+    return (
+        <div id="root">Arfhe Wallet</div>
+    )
+
+}
+
+export default App // Export etmeden başka bir componenentler kullanabilsin diye
