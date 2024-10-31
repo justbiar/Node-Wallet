980445f7bf3c1dff6fe34703090ea3ffaaef7b9b
just biar <160464890+justbiar@users.noreply.github.com>
2024-10-31 15:18:08 +0300

    .

--- nodeapp/src/App.jsx
+++ nodeapp/src/App.jsx
@@ -3,9 +3,13 @@ import arfLogo from './images/icon48.png'
 import './App.css'
 
 function App() {
+    let a = 15; // {} kullanarak return içine javascript kodları yerleştirebiliriz. 
     // return içine html dışına javascript yazılır.
     return (
-        <div id="root">Arfhe Wallet</div>
+        <div id="root"><p>Arfhe Wallet</p>
+            a değeri {a}
+
+        </div>
     )
 
 }
